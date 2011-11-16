5cbeefbee7a4c6413efd29fa72845250c028a5d0
Rob Hurring <robert.hurring@lexisnexis.com>
2011-11-16 14:34:24 -0500

    problem 5 alt

---
+++ problem_5-1.rb
@@ -0,0 +1,15 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# => 232792560
+
+range = (1..20)
+
+n = 0
+found = false
+
+until found
+  n += range.end
+  found = range.all?{ |i| n % i == 0 }
+end
+
+puts "N: #{n}"
