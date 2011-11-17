16c875d237b27c8e07c354df5940119c170e1bd4
Matt Mondok <matt.mondok@gmail.com>
2011-11-17 09:13:05 -0800

    Problem 6

---
+++ problem_6.rb
@@ -0,0 +1,21 @@
+# http://projecteuler.net/problem=6
+# The sum of the squares of the first ten natural numbers is,
+# 
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+# 
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+# 
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+sum_of_squares = 0
+square_of_sum = 0
+
+(1..100).each { |y| 
+  sum_of_squares = sum_of_squares + (y*y) 
+  square_of_sum = square_of_sum + y
+}
+square_of_sum = square_of_sum * square_of_sum
+
+puts  square_of_sum - sum_of_squares
