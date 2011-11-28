7a7ecf5f9a8b4810831c369a317467be12527bcb
Rob Hurring <robert.hurring@lexisnexis.com>
2011-11-28 14:29:16 -0500

    refactoring

--- problem_16-1.rb
+++ problem_16-1.rb
@@ -1,7 +1,5 @@
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # What is the sum of the digits of the number 2^1000?
-# => 1319
+# => 1366
 
-t = 0
-(2<<1000).to_s.each_char{ |c| t += c.to_i }
-puts t
+puts (2**1000).to_s.each_char.map(&:to_i).reduce(&:+)
